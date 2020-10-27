703900dfb9eea3c3a37c396ed3432cd1e0d4661f
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-10-26 21:48:16 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
-    <div>
-     :)
-    </div>
+    <>
+     <h1>Hello world</h1>
+     <GlobalStyle></GlobalStyle>
+    </>
   );
 }
 
