c73c375cdc1ecaea3cee842c38a7b5306dc326fc
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-11-17 13:54:58 -0300

    Adicionando e removendo Toasts

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,48 +1,25 @@
 import React from 'react';
 
-import { Container, Toast } from './styles';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import Toast from './Toast';
 
-const ToastContainer: React.FC = () => {
+import {ToastMessage} from '../../hooks/Toast';
+import { Container } from './styles';
+
+interface ToastContainerProps {
+    messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+
     return (
         <Container>
-            <Toast hasDescription>
-                <FiAlertCircle size={20}></FiAlertCircle>
-
-                <div>
-                    <strong>Aconteu um erro</strong>
-                    <p>Não foi possúvel ...</p>
-                </div>
-
-                <button type="button">
-                    <FiXCircle size={18}></FiXCircle>
-                </button>
-            </Toast>
-
-            <Toast type="success" hasDescription={false}>
-                <FiAlertCircle size={20}></FiAlertCircle>
-
-                <div>
-                    <strong>Aconteu um erro</strong>
-                </div>
-
-                <button type="button">
-                    <FiXCircle size={18}></FiXCircle>
-                </button>
-            </Toast>
-
-            <Toast type="error" hasDescription>
-                <FiAlertCircle size={20}></FiAlertCircle>
-
-                <div>
-                    <strong>Aconteu um erro</strong>
-                    <p>Não foi possúvel ...</p>
-                </div>
-
-                <button type="button">
-                    <FiXCircle size={18}></FiXCircle>
-                </button>
-            </Toast>
+            {messages.map(message => (
+                <Toast 
+                    key={message.id}
+                    message={message}
+                    >   
+                </Toast>
+            ))}
         </Container>
     );
 }
