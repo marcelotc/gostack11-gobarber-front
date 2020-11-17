33ba55090ccdd7a0eb4b68f63eaa1bea50a5be7a
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-11-17 18:47:20 -0300

    Finalizando App

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,7 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/Auth';
 import { useToast } from '../../hooks/Toast';
@@ -27,6 +27,8 @@ const SignIn: React.FC = () => {
     const { signIn } = useAuth();
     const { addToast } = useToast();
 
+    const history = useHistory();
+
     const handleSubmit = useCallback(async (data: SignInForm) => {
         try {
             formRef.current?.setErrors({});
@@ -47,6 +49,7 @@ const SignIn: React.FC = () => {
             email: data.email,
             password: data.password
         });
+        history.push('/dashboard');
         } catch (err) {
             if(err instanceof Yup.ValidationError) {
                 const errors = getValidationErrors(err);
